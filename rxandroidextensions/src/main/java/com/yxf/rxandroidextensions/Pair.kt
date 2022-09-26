85065565c6220f810d51127f01ae97be21d7d30b
Carl <1083223454@qq.com>
2022-09-26 15:18:18 +0800

    add three pair

--- rxandroidextensions/src/main/java/com/yxf/rxandroidextensions/Pair.kt
+++ rxandroidextensions/src/main/java/com/yxf/rxandroidextensions/Pair.kt
@@ -2,6 +2,9 @@ package com.yxf.rxandroidextensions
 
 import io.reactivex.Observable
 
+class ThreePair<F, S, T>(val first: F, val second: S, val third: T)
+
+
 fun <T, D> Observable<T>.pairWith(data: D): Observable<Pair<T, D>> {
     return map { Pair(it, data) }
 }
@@ -12,5 +15,17 @@ fun <T, D, R> Observable<Pair<T, D>>.map(block: (t: T, d: D) -> R): Observable<R
 
 fun <T, D, R> Observable<Pair<T, D>>.flatMap(block: (t: T, d: D) -> R): Observable<R> {
     return flatMap { Observable.just(block(it.first, it.second)) }
+}
+
+fun <F, S, T> Observable<F>.pairWith(second: S, third: T): Observable<ThreePair<F, S, T>> {
+    return map { ThreePair(it, second, third) }
+}
+
+fun <F, S, T, R> Observable<ThreePair<F, S, T>>.map(block: (f: F, s: S, t: T) -> R): Observable<R> {
+    return map { block(it.first, it.second, it.third) }
+}
+
+fun <F, S, T, R> Observable<ThreePair<F, S, T>>.flatMap(block: (f: F, s: S, t: T) -> R): Observable<R> {
+    return flatMap { Observable.just(block(it.first, it.second, it.third)) }
 }
 
